49b5bc41bdcc1579ac0609c9c259da520509ccb4
Tommy Haggerty <tommy.haggerty@gmail.com>
2016-10-28 13:45:06 -0600

    Updated student.rb
    * Adding validations for each field to verify that the field has
      been filled out, except the phone number portion where I allow
      blank.
    * Going to look into adding more validations
    
    [#133071487]

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,9 @@
 class Student < ApplicationRecord
+  validates :first_name, presence
+  validates :last_name, presence
+  validates :email, presence
+  # how to modify to_i to check for numericality and ignore "-"
+  validates :cell_number, presence, :allow_nil
+  validates :home_number, presence, :allow_nil
+  validates :work_number, presence, :allow_nil
 end
