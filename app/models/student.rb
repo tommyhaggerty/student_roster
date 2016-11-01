70baf709d62e99c0e226b85fbea9ea30edb52eb1
Tommy Haggerty <tommy.haggerty@gmail.com>
2016-11-01 11:42:14 -0600

    Updated student.rb validations
    * Adds a test to validate the length and format of the phone number.
      Displays a "Bad format" message if incorrect.
    
    [#133071487]

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,9 +1,9 @@
 class Student < ApplicationRecord
   validates :first_name, presence
   validates :last_name, presence
-  validates :email, presence, :format => /@/
+  validates :email, presence, format: { with: /@/}
   # how to modify to_i to check for numericality and ignore "-"
-  validates :cell_number, presence, :allow_nil, :format => /[0-9]/
-  validates :home_number, presence, :allow_nil, :format => /[0-9]/
-  validates :work_number, presence, :allow_nil, :format => /[0-9]/
+  validates :cell_number, presence, :allow_nil, format: { with: /\d{3}-\d{3}-\d{4}/, message: "bad format" }
+  validates :home_number, presence, :allow_nil, format: { with: /\d{3}-\d{3}-\d{4}/, message: "bad format" }
+  validates :work_number, presence, :allow_nil, format: { with: /\d{3}-\d{3}-\d{4}/, message: "bad format" }
 end
