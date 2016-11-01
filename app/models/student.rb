e953e642d2fe60585a588b1135edbad1b132021d
Tommy Haggerty <tommy.haggerty@gmail.com>
2016-11-01 11:21:53 -0600

    Updated student.rb in models
    * Added a validation on email to validate it has the @ symbol in
      the address.
    * Added a number regexp to the phone number lines to verify that
      there are numbers, need to refine further to include a test to
      validate no letters and that there are 10 numbers.
    
    [#133071487]

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,9 +1,9 @@
 class Student < ApplicationRecord
   validates :first_name, presence
   validates :last_name, presence
-  validates :email, presence
+  validates :email, presence, :format => /@/
   # how to modify to_i to check for numericality and ignore "-"
-  validates :cell_number, presence, :allow_nil
-  validates :home_number, presence, :allow_nil
-  validates :work_number, presence, :allow_nil
+  validates :cell_number, presence, :allow_nil, :format => /[0-9]/
+  validates :home_number, presence, :allow_nil, :format => /[0-9]/
+  validates :work_number, presence, :allow_nil, :format => /[0-9]/
 end
