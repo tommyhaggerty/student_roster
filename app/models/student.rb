19bffdc39ab80d349e1bee9abad722df3206f51f
Tommy Haggerty <tommy.haggerty@gmail.com>
2016-11-13 14:45:56 -0700

    Updated validations
    * Updated code to fix syntax errors with validating presence.
    * Dryed up the code a bit. Added a longer Regexp to validate emails.
    * Removed the validations of presence on items that allow_nil.
    
    [#133071487]

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,9 +1,8 @@
 class Student < ApplicationRecord
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :email, presence, format: { with: /@/}
+  validates :first_name, :last_name, presence: true
+  validates :email, presence: true, Format: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   # how to modify to_i to check for numericality and ignore "-"
-  validates :cell_number, presence, :allow_nil, format: { with: /\d{3}-\d{3}-\d{4}/, message: "bad format" }
-  validates :home_number, presence, :allow_nil, format: { with: /\d{3}-\d{3}-\d{4}/, message: "bad format" }
-  validates :work_number, presence, :allow_nil, format: { with: /\d{3}-\d{3}-\d{4}/, message: "bad format" }
+  validates :cell_number, :allow_nil, format: { with: /\d{3}-\d{3}-\d{4}/, message: "bad format" }
+  validates :home_number, :allow_nil, format: { with: /\d{3}-\d{3}-\d{4}/, message: "bad format" }
+  validates :work_number, :allow_nil, format: { with: /\d{3}-\d{3}-\d{4}/, message: "bad format" }
 end
